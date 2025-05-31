ef3868256fcbef55e45fff1e6f1a785375d984d3
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2025-05-31 12:05:39 +0500

    Write test for Reactive Forms

--- src/app/reactive-form/reactive-form.component.spec.ts
+++ src/app/reactive-form/reactive-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReactiveFormComponent } from './reactive-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ReactiveFormComponent', () => {
   let component: ReactiveFormComponent;
@@ -8,16 +9,32 @@ describe('ReactiveFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormComponent]
+      imports: [ReactiveFormComponent, ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ReactiveFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // View to Model Data flow.
+  it('should update the value of the input field', () => {
+    const input = fixture.nativeElement.querySelector('input');
+
+    const event = new Event('input');
+
+    input.value = 'Red';
+    input.dispatchEvent(event);
+
+    expect(component.favouriteColorControl.value).toEqual('Red');
   });
+
+  it('should update the value in the control', () => {
+    component.favouriteColorControl.setValue('Blue');
+
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input.value).toEqual('Blue');
+  });
+
 });
