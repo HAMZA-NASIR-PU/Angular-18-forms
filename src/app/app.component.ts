1a21dab9cf4e6e34395670644cb2aa9f83948ead
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2025-05-31 12:39:36 +0500

    Back to basics: Understanding the programattically change of value of FormControl in reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormComponent, TemplateFormComponent],
+  imports: [
+    ReactiveFormComponent,
+    TemplateFormComponent,
+    NameEditorComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
